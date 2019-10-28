50676dc600307ea6ef108cf33494a3a691eda9fe
Melike KILIC <melike180400@gmail.com>
2019-10-28 19:26:53 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,8 +7,8 @@ return array2.join(" ")
 end
 
 
-def reverse_each_word(array)
-  new_array = array.split(" ")
-  reversed_array = new_array.collect {|x| x.reverse!}
-  return reversed_array.join(" ")
+def reverse_each_word(sentence)
+  array1 = sentence.split(" ")
+  array2 = array1.collect {|x| x.reverse!}
+  return array2.join(" ")
 end
